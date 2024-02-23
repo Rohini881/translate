f2cb5bb80162ff752cf74540c9ffc0a10e7833e3
Rohini881 <84249719+Rohini881@users.noreply.github.com>
2024-02-24 00:55:18 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -14,7 +14,7 @@ function translateFunction(){
 
 async function translateText(enteredText, inputLanguage, outputLanguage){
     try{
-        let result = await fetch("https://libretranslate.de/translate", {
+        let result = await fetch("https://libretranslate/translate", {
             method: "POST",
             body: JSON.stringify({
                 q: enteredText,
