c6c6eba7bd774f18c356d3f79fa4bbd340d04adc
Rohini881 <84249719+Rohini881@users.noreply.github.com>
2024-02-23 23:49:42 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -27,5 +27,5 @@ app.post('/translate', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http: https://rohini881.github.io:${port}`);
 });
